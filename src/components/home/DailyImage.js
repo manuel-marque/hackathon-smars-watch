7bfd5b2280b9c21e7beebfc8dd5bdb0cf7f7f2de
Manuel Marque <manuelmarq05@gmail.com>
2021-05-11 18:04:29 +0200

    added the image and paths

--- src/components/home/DailyImage.js
+++ src/components/home/DailyImage.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import pictureDay from '../../img/car-media.jpg'
+import './dailyImage.css'
 
 export const DailyImage = () => {
     return (
-        <div> 
-            <img scr alt="photoDay" className="Photos"></img>
+        <div>
             <h3>Image Of the Day</h3>
+            <img src={pictureDay} alt="photoDay" className="photos"></img>
         </div>
     )
 }
