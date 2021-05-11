fd826f46bdfc38a22decd133a3e5f5644bc93d74
unknown <sergio-caballero_student2021@wilder.school>
2021-05-11 12:53:13 +0200

    added GlobalStyles

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import GlobalStyles from './components/GlobalStyles';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <GlobalStyles />
   </React.StrictMode>,
   document.getElementById('root')
 );
