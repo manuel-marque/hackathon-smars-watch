8af0453404fc7ba7dce7aee59a07299d58b786b7
unknown <sergio-caballero_student2021@wilder.school>
2021-05-12 09:37:50 +0200

    test

--- src/components/home/Entertainment.js
+++ src/components/home/Entertainment.js
@@ -32,7 +32,7 @@ export const Entertainment = ({isHappy}) => {
     let [isHovered, setIsHovered ]= useState(false)
     return (
         <StyledDiv onMouseEnter={() => setIsHovered(true)}
-        /*  */
+        /*a  */
         onMouseLeave={() => setIsHovered(false)}>
             {isHovered? <Img src={activity.img}></Img>: <Recommendation activity={activity}/>}
             
